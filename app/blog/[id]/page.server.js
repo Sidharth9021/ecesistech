8e5bff7bc6a581c2cecdd04edff6946c790177e1
Sidharth9021 <sidharth.ecesis@gmail.com>
2024-05-08 16:52:49 +0530

    first

--- app/blog/[id]/page.server.js
+++
@@ -1,7 +0,0 @@
-import data from "@/util/blog.json";
-
-// Loader function to fetch blog data based on the ID
-export async function loader({ params }) {
-    const blogPost = data.find(post => post.id.toString() === params.id);
-    return { blogPost };
-}
