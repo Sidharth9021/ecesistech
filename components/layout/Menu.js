cd1bbc2499a51e2e079e0de9a6cfecbd9ed6d24c
Sidharth9021 <sidharth.ecesis@gmail.com>
2024-05-08 17:12:53 +0530

    first

--- components/layout/Menu.js
+++ components/layout/Menu.js
@@ -17,32 +17,24 @@ export default function Menu() {
                     </ul> */}
                 </li>
                 <li className="menu-item-has-children"><Link href="#">Services</Link>
-                    <ul className="sub-menu">
+                    {/* <ul className="sub-menu">
                         <li><Link href="/services">Business Service</Link></li>
-                        {/* <li><Link href="/services-2">Finance Service</Link></li>
-                        <li><Link href="/services-3">Consulting Service</Link></li>
-                        <li><Link href="/services-4">Insurance Service</Link></li>
-                        <li><Link href="/services-5">Digital agency Service</Link></li> */}
                         <li><Link href="/services-details">Service Details One</Link></li>
                         <li><Link href="/services-details-2">Service Details Two</Link></li>
                         <li><Link href="/services-details-3">Service Details Three</Link></li>
                         <li><Link href="/services-details-4">Service Details Four</Link></li>
                         <li><Link href="/services-details-5">Service Details Five</Link></li>
-                    </ul>
+                    </ul> */}
                 </li>
                 <li className="menu-item-has-children"><Link href="#">Products</Link>
-                    <ul className="sub-menu">
-                        {/* <li><Link href="/team">Team One</Link></li>
-                        <li><Link href="/team-2">Team Two</Link></li>
-                        <li><Link href="/team-3">Team Three</Link></li> */}
+                    {/* <ul className="sub-menu">
+                      
                         <li><Link href="/team-4">Team Four</Link></li>
                         <li><Link href="/team-details">Team Details</Link></li>
                         <li><Link href="/project-details">Project Details</Link></li>
                         <li><Link href="/error">404 Error Page</Link></li>
-                        {/* <li><Link href="/login">Login Page</Link></li>
-                        <li><Link href="/register">Register Page</Link></li>
-                        <li><Link href="/forgot-password">Forgot password Page</Link></li> */}
-                    </ul>
+                        
+                    </ul> */}
                 </li>
                 {/* <li className="menu-item-has-children"><Link href="#">Blog</Link>
                     <ul className="sub-menu">
