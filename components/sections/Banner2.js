35cb8fe9815d17594071fa03917b8ff8b75e4d79
Sidharth9021 <sidharth.ecesis@gmail.com>
2024-05-09 16:09:51 +0530

    removed bg for mobile

--- components/sections/Banner2.js
+++ components/sections/Banner2.js
@@ -1,7 +1,7 @@
 'use client'
 import Link from 'next/link'
 import Banner from '../../public/assets/img/banner/banner.png'
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function Banner2() {
     const [isMobile, setIsMobile] = useState(false);
@@ -19,15 +19,19 @@ export default function Banner2() {
     return (
         <>
             <section className={`banner__area-two banner__bg-two object-cover ${isMobile ? "object-right" : ""}`} style={{
-                    backgroundImage: `url(${isMobile ? "" : "/assets/img/banner/banner.png"})`
-                }}>
+                backgroundImage: `url(${isMobile ? "" : "/assets/img/banner/banner.png"})`
+            }}>
                 <div className="container">
                     <div className="row">
-                    <div className={`col-lg-8 ${isMobile ? "mt-12" : ""}`}>
+                        <div className={`col-lg-8 ${isMobile ? "mt-10" : ""}`}>
                             <div className="banner__content-two mb-24">
-                                <h2 className="text-5xl font-bold" data-aos="fade-up" data-aos-delay={100}>Empowering Global Innovation</h2>
+                                <h2 className="text-5xl font-bold mb-5" data-aos="fade-up" data-aos-delay={100}>Empowering Global Innovation</h2>
                                 <h3 className='text-3xl text-black col-lg-10'>Your Partner in <span className="text-pink-400">Technology Transformation </span> and Customer-Centric <span className='text-blue-400'>Growth</span></h3>
-                                <Link href="#" className="btn border-btn mt-3" data-aos="fade-up" data-aos-delay={600}>Get Started</Link>
+                                {isMobile ?
+                                    <Link href="#" className="btn border-btn mt-40" data-aos="fade-up" data-aos-delay={600}>Get Started</Link>
+                                    :
+                                    <Link href="#" className="btn border-btn mt-10" data-aos="fade-up" data-aos-delay={600}>Get Started</Link>
+                                }
                             </div>
                         </div>
                     </div>
