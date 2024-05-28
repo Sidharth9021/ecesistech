2a71fede8e3ace33bc021524ab6c6c18c91710ad
Sidharth9021 <sidharth.ecesis@gmail.com>
2024-05-28 15:21:49 +0530

    added about us section

--- components/layout/Menu.js
+++ components/layout/Menu.js
@@ -1,8 +1,20 @@
+'use client'
 import Link from "next/link";
 import axios from "axios";
 import * as Sentry from "@sentry/nextjs";
+import React, { useEffect, useState } from "react";
+
 
 export default function Menu() {
+
+    const [activePath, setActivePath] = useState('');
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            // Set the active path from window.location.pathname
+            setActivePath(window.location.pathname);
+        }
+    }, []);
     // const testsentry = async () => {
     //     try {
     //         const response = await axios.post('http://192.168.2.159:8000/api/move_reject_folder/', { rejectedOptions, username: username });
@@ -18,10 +30,10 @@ export default function Menu() {
     return (
         <>
             <ul className="navigation">
-                <li className="active">
-                    <Link href="">Home</Link>
+                <li className={activePath === "/" ? "active" : ""}>
+                    <Link href="/">Home</Link>
                 </li>
-                <li>
+                <li className={activePath === "/about" ? "active" : ""}>
                     <Link href="/about">About Us</Link>
                 </li>
                 <li className="menu-item-has-children">
@@ -31,7 +43,7 @@ export default function Menu() {
                     <Link href="#">Products</Link>
                 </li>
                 <li>
-                    <Link href="#">contacts</Link>
+                    <Link href="#">Contacts</Link>
                 </li>
             </ul>
         </>
