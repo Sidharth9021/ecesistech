538c63167ec1167f10e248f122b76eb28e78eefc
Sidharth9021 <sidharth.ecesis@gmail.com>
2024-06-20 18:07:19 +0530

    added manpower

--- components/layout/Breadcrumb.js
+++ components/layout/Breadcrumb.js
@@ -22,16 +22,19 @@ export default function Breadcrumb({ breadcrumbTitle }) {
         {
             id: "About Us",
             backgroundImageUrl: "/assets/img/bg/breadcrumb_bg.jpg",
+            backgroundMobile : "/assets/img/bg/breadcrumb_bg.jpg",
             style: ""
         },
         {
             id: "Real Estate Valuations",
             backgroundImageUrl: "/assets/img/banner/real-estate-banner4.png",
+            backgroundMobile : "/assets/img/banner/mobile_static_real_estate.png",
             style: "text-white"
         },
         {
             id: "Manpower Staffing",
             backgroundImageUrl: "/assets/img/banner/manpower_banner2.png",
+            backgroundMobile : "/assets/img/banner/manpower_banner2.png",
             style: "text-black",
             img_style: "max-h-96"
         },
@@ -46,14 +49,22 @@ export default function Breadcrumb({ breadcrumbTitle }) {
         <>
             <section className={`breadcrumb__area breadcrumb__bg_real_estate`}>
        
-                <img src={isMobile ? "/assets/img/banner/mobile_banner2.png" :matchingBanner ? matchingBanner.backgroundImageUrl : "/assets/img/bg/breadcrumb_bg.jpg"} alt="" className={`absolute ${matchingBanner ? matchingBanner.img_style : ""} ${isMobile ? "h-full object-cover" : "object-fill"}  w-full -z-1 top-0 `} />
+                <img src={matchingBanner ? (isMobile ? matchingBanner.backgroundMobile :  matchingBanner.backgroundImageUrl) : "/assets/img/bg/breadcrumb_bg.jpg"} alt="" className={`absolute ${matchingBanner ? matchingBanner.img_style : ""} ${isMobile ? "h-full object-cover" : "object-fill"}  w-full -z-1 top-0 `} />
 
                     <div className="container">
 
                         <div className="row">
                             <div className="col-lg-6">
                                 <div className="breadcrumb__content">
-                                    <h2 className={`title ${matchingBanner ? matchingBanner.style : ""} ml-3 text-left`}>{breadcrumbTitle}</h2>
+                                    {
+                                        isMobile && breadcrumbTitle === "Real Estate Valuations" ? (
+                                            <h2 className={`title ${matchingBanner ? matchingBanner.style : ""} ml-3 text-left`}></h2>
+
+                                        ) : (
+                                            <h2 className={`title ${matchingBanner ? matchingBanner.style : ""} ml-3 text-left`}>{breadcrumbTitle}</h2>
+
+                                        )
+                                    }
                                     {/* <nav aria-label="breadcrumb">
                                     <ol className="breadcrumb">
                                         <li className="breadcrumb-item"><Link href="/">Home</Link></li>
