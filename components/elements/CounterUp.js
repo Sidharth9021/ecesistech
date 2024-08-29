d6e5a0b3d4f5acc09f3bf9092d873e02810f8272
Sidharth9021 <sidharth.ecesis@gmail.com>
2024-08-29 10:20:51 +0530

    fixed counter

--- components/elements/CounterUp.js
+++ components/elements/CounterUp.js
@@ -1,31 +1,35 @@
-'use client'
 import { useEffect, useState } from 'react'
 import Counter from './Counter'
 
 export default function CounterUp({ count }) {
     const [inViewport, setInViewport] = useState(false)
 
-    const handleScroll = () => {
+    const checkInViewport = () => {
         const elements = document.getElementsByClassName('odometer')
         if (elements.length > 0) {
             const element = elements[0]
             const rect = element.getBoundingClientRect()
-            const isInViewport = rect.top >= 0 && rect.bottom <= window.innerHeight
-            if (isInViewport && !inViewport) {
+            const isInViewport = (
+                rect.top >= 0 &&
+                rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+            );
+            if (isInViewport) {
                 setInViewport(true)
             }
         }
     }
 
     useEffect(() => {
-        window.addEventListener('scroll', handleScroll)
+        checkInViewport();  // Check on mount if it's already in the viewport
+        window.addEventListener('scroll', checkInViewport);
         return () => {
-            window.removeEventListener('scroll', handleScroll)
+            window.removeEventListener('scroll', checkInViewport);
         }
-    }, [])
+    }, []);
+
     return (
-        <>
-            <span className="odometer">{inViewport && <Counter end={count} duration={20} />}</span>
-        </>
-    )
+        <span className="odometer">
+            {inViewport ? <Counter end={count} duration={20} /> : null}
+        </span>
+    );
 }
