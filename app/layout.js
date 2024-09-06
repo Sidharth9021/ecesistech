27d936adea0122449816aefbad12c8566c4366c8
SidharthAjith <sidharth_ajith_k@ecesisgroups.com>
2024-09-06 16:07:12 +0530

    test

--- app/layout.js
+++ app/layout.js
@@ -9,6 +9,8 @@ import "/public/assets/css/swiper-bundle.css"
 import "/public/assets/css/aos.css"
 import "/public/assets/css/default.css"
 import "/public/assets/css/main.css"
+import Script from 'next/script';
+
 
 const inter = Inter({
     weight: ['300', '400', '500', '600', '700'],
@@ -32,15 +34,14 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <head>
-                <script>(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
+            <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-TBG2XGX4');</script>
             </head>
             <body className={`${inter.variable} ${outfit.variable}`}>
-                <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TBG2XGX4"
-                    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+          
                 {children}</body>
         </html>
     )
